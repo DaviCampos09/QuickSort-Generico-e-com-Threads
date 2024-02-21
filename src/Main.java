fed3b3cdd7def6003dba5b5bb2e5953edd3ab15a
Davi Campos <davimcampos09@gmail.com>
2024-02-21 09:34:31 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,20 @@ public class Main {
                 "Demetrice", "Azucena" };
         Integer[] arrayInt = {5, 9, 3, 7, 2, 8, 6, 1, 4, 23, 1, 13, 99, 101, 242, 72};        
 
+        //instaciando um objeto da classe QuickSort
         QuickSort<Integer> ordena = new QuickSort<>(arrayInt, 0, arrayInt.length-1);
 
+        //iniciando a thread
         ordena.start();
 
         try {
+            //aguarda a thread acabar
             ordena.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+        //imprimindo na tela o vetor ordenado
         System.out.println(java.util.Arrays.toString(arrayInt));
     }
 }
